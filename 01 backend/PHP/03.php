15ff9a26ee6a9c21c571e6a260202be3335a9727
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-07-25 16:34:37 -0300

    main

--- 01 backend/PHP/03.php
+++ 01 backend/PHP/03.php
@@ -1,5 +1,20 @@
------ arrays:
+----- condições:
 
------ concatenação:
+<?php
+    $var1 = 'João';
+    $var2 = 'Ana';
 
------ operações matemáticas:
+    if($var1 == $var2){
+        echo 'são iguais';
+    }else{
+        echo '<div style="width:100px;height:100px;backgouund:red;"></div>';
+        //esse resultado é um quadrado vermelho
+        //é possível implementar html tranquilamente
+    }
+?>
+
+----- operadores:
+
+----- operadores matemáticos:
+
+----- loopings:
