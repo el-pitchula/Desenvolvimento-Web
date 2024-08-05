2dd4afc682980bf1ad5829411e6a6ab2fc273e38
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-08-05 16:19:20 -0300

    main

--- 01 backend/BD e PHP/01.php
+++ 01 backend/BD e PHP/01.php
@@ -2,9 +2,17 @@
 - ativar Apache e MySQL no XAMPP (como sempre antes de programar)
 - primeiro configurar o phpMyAdmin por exemplo
 (onde os dados são organizado por tabelas e varios arquivos de BD)
-    - base de dados > criar tabela
+    - base de dados > criar tabela > add dados > guardar
+- conectar BD e php:
 
 - index.php
+<?php
+    $pdo = new PDO('mysql:host=localhost;dbname=nomedapastacomosarquivosdeBD','root','');
+    //parâmetros: 'local dos arquivos de BD','login php Admin','senha'
+    //se não aparecer nenhuma mensagem ele está funcionando
+?>
+
+- Insert PDO (para inserir)
 <?php
 
 ?>
