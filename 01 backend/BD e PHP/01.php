16fe836c7143b72f6f7a17ac0d679ccff704fc9a
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-08-05 16:09:25 -0300

    main

---
+++ 01 backend/BD e PHP/01.php
@@ -0,0 +1,10 @@
+----- Utilizando POO para criação de Banco de dados -----
+- ativar Apache e MySQL no XAMPP (como sempre antes de programar)
+- primeiro configurar o phpMyAdmin por exemplo
+(onde os dados são organizado por tabelas e varios arquivos de BD)
+    - base de dados > criar tabela
+
+- index.php
+<?php
+
+?>
