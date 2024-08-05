9677d1cc32c1fe51787d11b8ebea5c32c4ccc234
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-08-05 16:00:56 -0300

    main

--- 01 backend/PHP orientado a objetos/03_DesignPaterns.php
+++ 01 backend/PHP orientado a objetos/03_DesignPaterns.php
@@ -1,3 +1,61 @@
------ Design Paterns (Padrões de Projeto)
-- 
+----- Design Patterns (Padrões de Projeto)
+- Padrão de projeto: factory (fabricar)
+<?php
+    class Cachorro{
+        public function __construct(){
+            echo 'classe cachorro';
+        }
+    }
+    class Gato{
+        public function __construct(){
+            echo 'classe gato';
+        }
+    }
+    class Animal{
+        public static function build($nomeClasse){  //simple factory
+            return new $nomeClasse;
+        }
+    }
 
+    //new Gato();  //antes de implementar o factory
+    Animal::build('cachorro');  //simple factory
+?>
+
+- Facade Pattern (fachada)
+    - exemplo de um carrinho de compras:
+        - add/fechar carrinho
+        - calcular frete
+        - fechar pedido
+<?php
+    class Carrinho{
+        public function fecharCarrinho(){
+            echo 'carrinho fechado';
+        }
+    }
+    class Frete{
+        public function calcularFrete(){
+            echo 'frete calculado';
+        }
+    }
+    class Pedido{
+        public function fecharPedido(){
+            echo 'pedido fechado';
+        }
+    }
+    class Loja{
+        public function finalizarCompra(){
+            $this->FecharCarrinho();
+            $this->CalcularFrete();
+            $this->FecharPedido();
+        }
+        public function fecharCarrinho(){
+            $carrinho = new Carrinho();
+            $carrinho->fecharCarrinho();
+        }
+    }
+    $loja = new Loja();
+    $loja->finalizarCompra();
+?>
+
+- Singleton Pattern (único)
+- Adapter Pattern (adaptador)
