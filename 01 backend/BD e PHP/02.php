6ae3337b980ce7100331f74e5f0b0d5631945af1
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-08-06 16:02:03 -0300

    main

--- 01 backend/BD e PHP/02.php
+++ 01 backend/BD e PHP/02.php
@@ -74,8 +74,20 @@ criado anteriormente
     }
 ?>
 
-- queries mais avançadas: INNER JOIN ()
-
+- outros parâmetros: GROUPE, ORDER e LIMIT
 <?php
+    $pdo = new PDO('mysql:host=localhost;dbname=nomedapastacomosarquivosdeBD','root','');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = $pdo->prepare("SELECT * FROM `clientes` GROUP BY cargo ORDER BY nome ASC LIMIT 3");
+    //seleciona nomes da tabela clientes de mesmo cargo (def por cargo 1 e 2)
+    //para mostrar os nomes em ordem crescente e com limite de 3 nomes
+    $sql->execute();
+    $clientes = $sql->fetchAll();
 
+    foreach ($clientes as $key => $value){
+        echo $value['nome'];
+        echo '<hr>';
+    }
 ?>
+
+- queries mais avançadas: JOIN (introduzir em exemplos mais avançados)
