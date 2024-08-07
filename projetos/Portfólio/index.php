32a81686f325fcdcee10ce07a064761c52b9cc92
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-08-07 15:27:08 -0300

    main

--- projetos/Portfólio/index.php
+++ projetos/Portfólio/index.php
@@ -1,2 +1,12 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <title>Postfólio</title>
+    <meta name="viweport" content="width=devise-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <!-- para se tornar responsivo (ou seja, se adapta em qualquer tela) -->
+</head>
 
+<body>
+</body>
+</html>
